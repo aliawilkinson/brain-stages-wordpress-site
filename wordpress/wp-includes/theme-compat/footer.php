ad91c6b6babb019ba38035580d31a8d4d75ba28d
Alia Wilkinson <aliawilkinson@gmail.com>
2018-10-13 12:38:28 -0700

    edited content, photos, copy

--- wordpress/wp-includes/theme-compat/footer.php
+++ wordpress/wp-includes/theme-compat/footer.php
@@ -32,9 +32,6 @@ _deprecated_file(
 </div>
 </div>
 
-<!-- Gorgeous design by Michael Heilemann - http://binarybonsai.com/kubrick/ -->
-<?php /* "Just what do you think you're doing Dave?" */ ?>
-
 		<?php wp_footer(); ?>
 </body>
 </html>
